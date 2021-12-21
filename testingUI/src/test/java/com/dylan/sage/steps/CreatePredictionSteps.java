dfa1f555e4f3ac6e4a7aa59c6b08c1927d448d84
DylanRigney <84356573+DylanRigney@users.noreply.github.com>
2021-12-21 14:33:34 -0800

    Created all step files, added steps other testing

--- testingUI/src/test/java/com/dylan/sage/steps/CreatePredictionSteps.java
+++ testingUI/src/test/java/com/dylan/sage/steps/CreatePredictionSteps.java
@@ -1,57 +1,56 @@
 package com.dylan.sage.steps;
 
-import com.dylan.sage.pages.SageSignIn;
+import com.dylan.sage.pages.CustomPrediction;
 import com.dylan.sage.runners.SageRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 
-public class SignInStepImpl {
+public class CreatePredictionSteps {
 
-    public static SageSignIn sageSignIn = SageRunner.signIn;
+    public static CustomPrediction customPredictionPage = SageRunner.customPredictionPage;
     public static WebDriver driver = SageRunner.driver;
 
-    @Given("User is on the sign in page")
-    public void user_is_on_the_sign_in_page() {
+    @Given("User is on the landing view")
+    public void user_is_on_the_landing_view() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("User enters invalid login information")
-    public void user_enters_invalid_login_information() {
+    @When("User navigates to the create a prediction view")
+    public void user_navigates_to_the_create_a_prediction_view() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("User types the correct username")
-    public void user_types_the_correct_username() {
+    @When("Enters in prediction information")
+    public void enters_in_prediction_information() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("User types in the correct password")
-    public void user_types_in_the_correct_password() {
+    @When("Presses the Predict button")
+    public void presses_the_predict_button() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("User presses sign in")
-    public void user_presses_sign_in() {
+    @Then("A popup appears saying your prediction has been created")
+    public void a_popup_appears_saying_your_prediction_has_been_created() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("An error message is displayed")
-    public void an_error_message_is_displayed() {
+    @Then("User is back at the landing page")
+    public void user_is_back_at_the_landing_page() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("User has successfully logged in")
-    public void user_has_successfully_logged_in() {
+    @Then("Created prediction is displayed on landing page")
+    public void created_prediction_is_displayed_on_landing_page() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
 }
--- testingUI/target/test-classes/com.dylan.sage.features/ResolvedPredictionView.feature
+++ testingUI/target/test-classes/com.dylan.sage.features/ResolvedPredictionView.feature
@@ -1,5 +1,5 @@
 Feature: Resolved Prediction View
-
+  
   Scenario: User views a resolved prediction
     Given User is at the Resolved Prediction View
     When User selects a resolved prediction
